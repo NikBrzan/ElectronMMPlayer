0b6587ff1e828ed8dd97a4c3ca4c6a93275e39a8
NikBrzan <nik.brzan@student.um.si>
2025-05-27 19:08:34 +0200

    basic view and added preload.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1600,
-    height: 900
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
